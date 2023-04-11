7997f3c582bb27ef8a6f66f2b70fa3451ece3d62
Jean Casimiro <casimirojean2001@gmail.com>
2023-04-11 02:50:15 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('form-agenda')
+let addLinha = '';
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNome = document.getElementById('nome')
+    const inputTelefone = document.getElementById('telefone')
+
+    let linha = '<tr>'
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    addLinha += linha;
+
+    const lista = document.querySelector('tbody');
+    lista.innerHTML = addLinha;
+})
